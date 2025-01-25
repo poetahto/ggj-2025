a79995cf9183b5367d31bdfb20d4e5d11f6e5ec7
Daniel Walls <walls.dk@gmail.com>
2025-01-24 22:21:51 -0800

    Actually add input system, create scripts folder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed;
+
+    private void Update()
+    {
+        
+    }
+}
