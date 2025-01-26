e3e39a0aeffac878be6d644b0009c71cca8b00a1
Daniel Walls <walls.dk@gmail.com>
2025-01-26 01:28:34 -0800

    added warp triggers to every scene

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class Trigger : MonoBehaviour
+    {
+        public UnityEvent onTrigger;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+                onTrigger.Invoke();
+        }
+    }
+}
