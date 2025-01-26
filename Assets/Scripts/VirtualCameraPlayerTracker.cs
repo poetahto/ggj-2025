fddaf7836b38b54683c63fe1f6e8d17c46eea7b0
Daniel Walls <walls.dk@gmail.com>
2025-01-26 10:58:49 -0800

    added camera to scenes, some small bug fixes

---
+++ Assets/Scripts/VirtualCameraPlayerTracker.cs
@@ -0,0 +1,18 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    public class VirtualCameraPlayerTracker : MonoBehaviour
+    {
+        private CinemachineVirtualCamera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<CinemachineVirtualCamera>();
+            GameObject player = GameObject.FindWithTag("Player");
+            _camera.LookAt = player.transform;
+        }
+    }
+}
