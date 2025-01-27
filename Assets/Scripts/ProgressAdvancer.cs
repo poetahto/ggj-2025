786965399be9f5131c35082e09808a8b37dfb7a1
Daniel Walls <walls.dk@gmail.com>
2025-01-26 18:07:00 -0800

    interactions

---
+++ Assets/Scripts/ProgressAdvancer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ProgressAdvancer : MonoBehaviour
+    {
+        public int progressCount;
+        
+        public void Advance()
+        {
+            GlobalState state = GlobalState.GetInstance();
+            
+            if (state.ProgressCounter < progressCount)
+                state.ProgressCounter = progressCount;
+        }
+    }
+}
---
+++ Assets/Scripts/ProgressEnabler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ProgressEnabler : MonoBehaviour
+    {
+        public int minimumProgress;
+        public bool isActive;
+
+        private void Start()
+        {
+            gameObject.SetActive(GlobalState.GetInstance().ProgressCounter >= minimumProgress ? isActive : !isActive);
+        }
+    }
+}
